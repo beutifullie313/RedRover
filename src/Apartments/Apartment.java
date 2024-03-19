81aacbfa4087d6e3e8fd4d0eece99008b95b227d
Yuliya <jsapego777@gmail.com>
2024-03-19 15:37:22 -0400

    price name changed

--- src/Apartments/Apartment.java
+++ src/Apartments/Apartment.java
@@ -6,13 +6,13 @@ public class Apartment {
         // this is a schema of future objects = class
         int bedrooms;
         Address address;
-        int price;
+        int price1;
         int area;
 
         String getInfoCard() {
                 String result =
                 "\n___________" +
-                        "\nPrice: " + price +
+                        "\nPrice: " + price1 +
                         "\nBedrooms: " + bedrooms +
                         "\nCity: " + address.city +
                         "\nState: " + address.state +
--- src/Apartments/Apartment_1.java
+++ src/Apartments/Apartment_1.java
@@ -12,28 +12,28 @@ public class Apartment_1 {
         apt1.address = new Address();
         apt1.address.city = "San Francisco";
         apt1.address.state = "CA";
-        apt1.price = 3500;
+        apt1.price1 = 3500;
 
         Apartment apt2 = new Apartment();
         apt2.bedrooms = 4;
         apt2.address = new Address();
         apt2.address.city = "Atlanta";
         apt2.address.state = "GA";
-        apt2.price = 2500;
+        apt2.price1 = 2500;
 
         Apartment apt3 = new Apartment();
         apt3.bedrooms = 5;
         apt3.address = new Address();
         apt3.address.city = "Fresno";
         apt3.address.state = "CA";
-        apt3.price = 1500;
+        apt3.price1 = 1500;
 
         Apartment apt4 = new Apartment();
         apt4.bedrooms = 5;
         apt4.address = new Address();
         apt4.address.city = "San Francisco";
         apt4.address.state = "CA";
-        apt4.price = 2500;
+        apt4.price1 = 2500;
 
 
         Apartment[] apts = {apt1, apt2, apt3, apt4};
@@ -51,7 +51,7 @@ public class Apartment_1 {
                 // I want to find apts not more than 2500$
 
                 for (int i = 0; i < apts.length; i++) {
-                    if (apts[i].price <= 2500) {
+                    if (apts[i].price1 <= 2500) {
                         System.out.println(apts[i].getInfoCard());
 
                     }
