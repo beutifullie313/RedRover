586bad73aa98a3f26fcc596f1318bdd8f1bb2ee8
beutifullie313 <jsapego777@gmail.com>
2024-04-11 13:20:41 -0400

    add all Java files

--- src/PersonClassUsage.java
+++ src/PersonClassUsage.java
@@ -1,27 +1,27 @@
-public class PersonClassUsage {
-    public static void main(String[] args) {
-
-        //THIS IS SPECIFIC PERSON
-        // Inctance of class
-//        Person_Class person1 = new Person_Class();
+//public class PersonClassUsage {
+//    public static void main(String[] args) {
 //
-//        person1.name = "Sergei";
-//        person1.age = 20;
-
-//        Person_Class p2 = new Person_Class();
-//        p2.Person_Class.name = "Anna";
-//        p2.age = 30;
-
-// use constructor
-        Person_Class p1 = new Person_Class("Swergei", 30);
-
-        p1.setName("Ivan");
-
-        System.out.println(p1.getName());
-
-     Worker worker = new Worker("Ivan", 40, 23400){
-
-     }
+//        //THIS IS SPECIFIC PERSON
+//        // Inctance of class
+////        Person_Class person1 = new Person_Class();
+////
+////        person1.name = "Sergei";
+////        person1.age = 20;
+//
+////        Person_Class p2 = new Person_Class();
+////        p2.Person_Class.name = "Anna";
+////        p2.age = 30;
+//
+//// use constructor
+//        Person_Class p1 = new Person_Class("Swergei", 30);
+//
+//        p1.setName("Ivan");
+//
+//        System.out.println(p1.getName());
+//
+//     Worker worker = new Worker("Ivan", 40, 23400){
+//
+//     }
 
 
 
@@ -34,6 +34,6 @@ public class PersonClassUsage {
 //        work.age = 35; // from person
 //        work.salary = 24000; // from worker
 //
-//        System.out.println((work.getName()));
-    }
-}
+////        System.out.println((work.getName()));
+//    }
+//}
